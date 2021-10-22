3dfe4c66be12be09e9fdabd5897e29f6b926b8a3
Kshitij Bhosale <kshitijbhosale876@gmail.com>
2021-10-22 20:36:46 +0530

    Initial Commit

---
+++ Dog ImagesUITests/Dog_ImagesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dog_ImagesUITestsLaunchTests.swift
+//  Dog ImagesUITests
+//
+//  Created by Kshitij Bhosale on 22/10/21.
+//
+
+import XCTest
+
+class Dog_ImagesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
