2b2fd0c30208d30299c4a7711c39e17cf29ebd54
Kshitij Bhosale <kshitijbhosale876@gmail.com>
2021-10-23 22:38:13 +0530

    Commit

--- Dog Images/ViewController.swift
+++ Dog Images/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showDogImages: UIButton!
+    @IBOutlet weak var dogImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
